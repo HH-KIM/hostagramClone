cd8cc2eb4dcdc716cd3fe2f3ad906b8ac2358d67
h2k9161 <h2k9161@gmail.com>
2018-07-29 22:11:50 +0900

    image model column add

--- hostagram/hostagram/images/models.py
+++ hostagram/hostagram/images/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from hostagram.users import models as user_models
 # Create your models here.
 
 
@@ -16,9 +16,16 @@ class Image(TimeStampedModel):
         file = models.ImageField()
         location = models.CharField(max_length=140)
         caption = models.TextField()
+        creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT)
 
 
 class Comment(TimeStampedModel):
 
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null = True)
+    image = models.ForeignKey(Image , on_delete=models.PROTECT, null = True)
 
+class Like(TimeStampedModel):
+
+    creator = models.ForeignKey(user_models.User , on_delete=models.PROTECT, null = True)
+    image = models.ForeignKey(Image , on_delete=models.PROTECT, null = True)
